c07cfd899a98a56aeab65489142bcc27ddd4be6f
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-05-29 09:32:17 +0500

    Add item function is done.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Arrow from "../assets/arrow-right.svg";
 import Bit from "../assets/BitBucket.svg";
 import Drop from "../assets/Dropbox.svg";
@@ -12,6 +12,16 @@ import Add from "../assets/add-square.svg";
 import Form from "./Form";
 
 const Home = () => {
+  const [items, setItems] = useState([{ id: Date.now(), name: "", key: "" }]);
+
+  function handleAddItem() {
+    let obj ={
+      id: Date.now(),
+      name: '',
+      key: ''
+    };
+    setItems([...items, obj])
+  }
   return (
     <>
       <div className="top-header flex justify-between border-b-2 pb-4">
@@ -42,13 +52,19 @@ const Home = () => {
       </div>
       <div className="media border border-b-2 mt-4 p-6 bg-[#faf9f9] rounded-md">
         <h1 className=" text-xl font-semibold mb-4">Loyiha ketma-ketligi</h1>
-        <Form />
+
+        {items.map((el, index) => {
+          return <Form key={index} data={el} />;
+        })}
+
         <div className="footer px-4 flex justify-between">
-          <p className="flex gap-2 cursor-pointer w-[130px] items-center">
+          <p className="flex gap-2 cursor-pointer w-[130px] items-center" onClick={handleAddItem}>
             <img src={Add} alt="" className="cursor-pointer" />
             <span className="font-semibold text-blue-800">ustun qo'shish</span>
           </p>
-          <button className="bg-blue-700 px-6 py-2 rounded-lg text-white ">Saqlash</button>
+          <button className="bg-blue-700 px-6 py-2 rounded-lg text-white ">
+            Saqlash
+          </button>
         </div>
       </div>
     </>
